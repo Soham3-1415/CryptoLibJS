1e129280dcf65d9c3ab1c023a8b7749c871aedcd
Soham Roy (keybase.io/sohamroy) <soham3.1415@gmail.com>
2019-01-09 15:21:50 -0600

    Changed key output

--- RSAKeygen.js
+++ RSAKeygen.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const elems = document.querySelectorAll('select');
 	const instances = M.FormSelect.init(elems, {});
-
 });
 
 const form  = document.getElementsByTagName('form')[0];
 const bits = document.getElementById('bits');
 const pubKey = document.getElementById('pubkey');
 const privKey = document.getElementById('privkey');
+const output = document.getElementById('output');
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
+	output.style.display = "block";
 	const keypair = RSA.keyGen(bits.value);
 	privKey.value = keypair[0];
 	pubKey.value = keypair[1];
