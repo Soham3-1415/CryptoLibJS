f3931dec6e6741f0b4ad3e97c05c20eda68e424b
Soham Roy (keybase.io/sohamroy) <soham3.1415@gmail.com>
2019-01-11 14:56:40 -0600

    Added output for processing time and changed titles of the pages.

--- RSAKeygen.js
+++ RSAKeygen.js
@@ -8,10 +8,13 @@ const bits = document.getElementById('bits');
 const pubKey = document.getElementById('pubkey');
 const privKey = document.getElementById('privkey');
 const output = document.getElementById('output');
+const generating = document.getElementById('generating');
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
-	output.style.display = "block";
+	generating.style.display = "block";
 	const keypair = RSA.keyGen(bits.value);
 	privKey.value = keypair[0];
 	pubKey.value = keypair[1];
+	generating.style.display = "none";
+	output.style.display = "block";
 }, false);
